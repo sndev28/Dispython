bd69d768b025b1317e6db6d79922513d93c709c4
sndev28 <niyas.shamil@gmail.com>
2021-03-24 09:56:07 +0530

    Ver 2.10.32 -> Working code now. Client not working

--- dispython.py
+++ dispython.py
@@ -20,21 +20,56 @@
 
 
 import discord
-from discord.ext import COMMANDS
+from discord.ext import commands
 import os
+import asyncio
+import importlib
+import file
+import requests
 
-
-
+global client
 client = commands.Bot(command_prefix = 'd!')
 
 
+async def run(ctx):
+    try :
+        importlib.reload(file)
+        await file.script(ctx)
+
+    except Exception as err:
+        await ctx.send(str(err))
+
+
+
 @client.command()
+async def runcode(ctx):
+
+
+    def check(msg):
+        return msg.author == ctx.author and msg.channel == ctx.channel
 
 
 
+    await ctx.send("`Enter the entire code you want to test:`")
 
+    try:
+        scriptres = await client.wait_for("message", check = check, timeout = 300.0)
 
+    except asyncio.TimeoutError:
+        await ctx.send("`TimeOut`")
+        return
 
+    scriptlines = scriptres.content.split("\n")
+
+    scriptfunc = "async def script(ctx):\n"
+
+    for line in scriptlines:
+        scriptfunc = scriptfunc + "    " + line + "\n"
+
+    with open('file.py', 'w') as file:
+        file.write(scriptfunc)
+
+    asyncio.create_task(run(ctx))
 
 
 
